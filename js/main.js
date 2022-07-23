5cf59b4c91231189cc5fddd4c2af35e96907b855
unknown <ing.ramirozs@gmail.com>
2022-07-23 18:54:57 -0500

    creando funciones para eventos click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-let btnlogin = document.querySelector(".btnlogin");
+let Inicio = document.querySelector(".logo")
+let BtnLogin = document.querySelector(".btnlogin")
+let BtnConsola = document.querySelector(".btnconsola")
 
-btnlogin.addEventListener("click", function(){
-window.open( "./login.html","_blank")
+function Openpage(url){
+    window.location = url
+}
+
+Inicio.addEventListener("click",function(){
+    Openpage("./index.html")
+});
+
+BtnLogin.addEventListener("click",function(){
+    Openpage("./login.html")
+});
+
+BtnConsola.addEventListener("click",function(){
+    Openpage("./productos.html")
 })
+
+
+
 
