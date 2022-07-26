aeb96e33376873c54a9910a29814e64facbea118
unknown <ing.ramirozs@gmail.com>
2022-07-25 22:32:53 -0500

    agregando comentarios al codigo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let Inicio = document.querySelector(".logo")
 let BtnLogin = document.querySelector(".btnlogin")
 let BtnConsola = document.querySelector(".btnconsola")
+let vistalista = document.querySelector(".div_lista")
+let inputsearch = document.querySelector(".searchbar")
+let vistaform = document.querySelector(".formu")
+
 const ListaProductos = ["consola","nintendo","play","xbox","control","laptop","celular","start","funko"]
 
 function Openpage(url){
@@ -19,7 +23,7 @@ BtnConsola.addEventListener("click",function(){
     Openpage("./productos.html")
 })
 
-
+//creacion del div en el DOM
 let div = document.createElement("div");
 div.className="div_lista";
 let ul = document.createElement("ul");
@@ -34,7 +38,7 @@ li.textContent= ""
 let cajadeldom= document.querySelector(".box_search");
 cajadeldom.appendChild(div);
 
-
+//funcion para buscar el valor en el array y mostrarlo en el div creado
 function searchbar(array){
     let liElemet = "" ;
     for (let i= 0; i < array.length; i++) {
@@ -50,6 +54,7 @@ function searchbar(array){
     searchbar(filterNames);
  }
 
+ //funciones ocultar o mostrar  
 function ocultar(ocu){
     ocu.style.display= "none"
 }
@@ -58,10 +63,7 @@ function mostrar(show){
     show.style.display = "block"
 }
 
-let vistalista = document.querySelector(".div_lista")
-let inputsearch = document.querySelector(".searchbar")
-let vistaform = document.querySelector(".formu")
-
+//eventos mostrar u ocultar el div cuando se hace clik  se mueve en el div
 inputsearch.addEventListener('click',function(){
 mostrar(vistalista)
 })
