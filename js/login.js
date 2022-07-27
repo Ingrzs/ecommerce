b293bf2ddf8c560ce3b2edacc656105ffef9f933
unknown <ing.ramirozs@gmail.com>
2022-07-27 15:14:44 -0500

    agregando funcion para guardar usermail en sessionstorage

--- js/login.js
+++ js/login.js
@@ -1,28 +1,37 @@
-
-
-
-
-/* Creating an array with an object inside. */
+let btnlogin = document.querySelector(".btnlog");
+let usermail = document.querySelector(".usermail");
+let userpassword = document.querySelector(".userpassword"); 
 let user = { name:"ramiro",mail:"por@por.com",password :"this"}
 
-let btnlogin = document.querySelector(".btnlog")
-let formulario = document.querySelector("formulariologin")
-
-function validar(){
-    var usermail = document.querySelector(".usermail").value;
-    var userpassword = document.querySelector(".userpassword").value;  
-    if(usermail == user.mail && userpassword == user.password){
-         window.open("./index.html","_self");
-    }
+function validar(a,b){
+    if(a == user.mail && b == user.password){
+       let  Backuser = a
+        return Backuser; 
+        }
     else{
-        alert("incorrecto")
-         clear();
+        alert("Datos incorrectos")
+        return false;
         }
 }
 
+function saveuser(c){
+    sessionStorage.setItem('key',c)
+    let Backuser = sessionStorage.getItem('key');
+    return Backuser;
+}
 
 
-btnlogin.addEventListener("click",function(){
-validar();
-   
-})
+btnlogin.addEventListener("click", function(){
+let savedate = validar(usermail.value,userpassword.value);
+console.log(saveuser(savedate))
+}) 
+
+
+
+if (savedate === usermail.value){
+    location.href = "./index.html"
+ }
+ else{
+    location.reload()
+ }
+ 
