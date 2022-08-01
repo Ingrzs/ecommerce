55f0b9341a3077524626975ef314178bf2b85c88
unknown <ing.ramirozs@gmail.com>
2022-07-31 19:20:46 -0500

    añadiendo funciones de controllerlogin para storage

--- js/productos.js
+++ js/productos.js
@@ -1,5 +1,20 @@
+import { CheckStorage } from "./controllerlogin.js"
+import { DeleteStorage } from "./controllerlogin.js"
+
 let Agregarproducto = document.querySelector(".btnadd")
+let Showuser = document.querySelector(".showuser");
+let Closesesion = document.querySelector(".cerrarsesion")
+let Boxdatalogin = document.querySelector(".box_datalogin")
+let Getstorage = localStorage.getItem('key')
+Showuser.innerHTML = localStorage.getItem('key')
 
 Agregarproducto.addEventListener("click",function(){
     location.href = "./addproducto.html"
 })
+
+Closesesion.addEventListener('click',function(){
+   DeleteStorage()  
+   location.reload()
+})
+
+CheckStorage(Getstorage,Boxdatalogin,Agregarproducto);
