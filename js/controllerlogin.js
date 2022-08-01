5a32c5ef8cb2b45cd8aa3b256943ce974e2308da
unknown <ing.ramirozs@gmail.com>
2022-07-31 22:01:27 -0500

    agregando funcion storage para pagina productos

--- js/controllerlogin.js
+++ js/controllerlogin.js
@@ -10,5 +10,12 @@ function DeleteStorage(){
 localStorage.removeItem('key');
 }
 
+function CheckStorageProducto(a,b,c){
+    if (a.length > 0){
+        b.style.display = "block"
+        c.style.display = "block"
+     } 
+}
 export  { CheckStorage };
 export { DeleteStorage };
+export { CheckStorageProducto};
