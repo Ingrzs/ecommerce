71d41f289e601e7e8aba96eb9e32e2e71fbbdaf3
unknown <ing.ramirozs@gmail.com>
2022-08-09 23:08:27 -0500

    agrega funcion autocomplete

--- js/productos.js
+++ js/productos.js
@@ -1,5 +1,7 @@
 import { CheckStorageProducto } from "./controllerlogin.js"
 import { DeleteStorage } from "./controllerlogin.js"
+import { Cambiaralhacerclick} from "./autocomplete.js"
+import {selectItem} from "./autocomplete.js"
 
 let Agregarproducto = document.querySelector(".btnadd")
 let Showuser = document.querySelector(".showuser");
@@ -7,6 +9,13 @@ let Closesesion = document.querySelector(".cerrarsesion")
 let Boxdatalogin = document.querySelector(".box_datalogin")
 let Getstorage = localStorage.getItem('key')
 Showuser.innerHTML = localStorage.getItem('key')
+
+let div_resultados = document.querySelector(".div_lista")
+let inputsearch = document.querySelector(".inputsearch")
+
+inputsearch.addEventListener('input', Cambiaralhacerclick);
+div_resultados.addEventListener('click' ,selectItem)
+
 
 Agregarproducto.addEventListener("click",function(){
     location.href = "./addproducto.html"
